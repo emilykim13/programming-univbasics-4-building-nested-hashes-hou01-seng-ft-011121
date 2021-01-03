bc1ff92c9662d0d355f9c90f22f8cdb961aca509
Emily Nguyen <emilyn0614@gmail.com>
2021-01-03 18:31:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -78,7 +78,7 @@ monopoly =  {
    :names=> 
    {
      :reading_railroad=>
-     {:mortgage_value},
+     {:mortgage_value=>100},
      :pennsylvania_railroad=>
      {},
      :b_and_o_railroad=>
