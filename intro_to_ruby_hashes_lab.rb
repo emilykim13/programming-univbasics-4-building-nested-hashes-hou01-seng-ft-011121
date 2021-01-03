539cf1eba208cbb28246a5dc4dc1d99dcd2a02aa
Emily Nguyen <emilyn0614@gmail.com>
2021-01-03 18:27:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,6 +32,18 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 
+monopoly =  {
+  :railroads=> 
+{
+    :pieces=>4,
+    :rent_in_dollars=>
+    {
+      :one_
+    },
+   :names=> 
+   {}
+}
+}
 end
 
 def monopoly_with_fourth_tier
